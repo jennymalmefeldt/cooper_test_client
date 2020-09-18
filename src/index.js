5aa3d5ef6bc62817e974fe3dab2553a6896ef91b
Connor Roelofsen <connorroelofsen15@gmail.com>
2020-09-18 08:07:31 +0200

    Adds axios with yarn, configures axios in index.js.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import axios from "axios";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let apiUrl;
+if (process.env.NODE_ENV === "production") {
+  apiUrl = "https://cr-cooper-api.herokuapp.com/api/vi"
+} else {
+  apiUrl = "http:/localhost:3000/api/v1";
+}
+axios.defaults.baseURL = apiUrl
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 serviceWorker.unregister();
