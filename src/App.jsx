1accd9b9f205ef2e36e794b17046f6cd501b0de1
Connor Roelofsen <connorroelofsen15@gmail.com>
2020-09-18 21:11:41 +0200

    Rewrote app.jsx and attempts to fix auth.js.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
 import DisplayCooperResult from "./components/DisplayCooperResult";
 import InputFields from "./components/InputFields";
 import LoginForm from "./components/LoginForm";
@@ -11,7 +11,7 @@ class App extends Component {
     age: "",
     renderLoginForm: false,
     authenticated: false,
-    message: ""
+    message: "",
   };
 
   onChangeHandler = e => {
@@ -32,38 +32,22 @@ class App extends Component {
   };
 
   render() {
-    const { renderLoginForm, authenticated, message } = this.state;
-    let renderLogin;
-    switch (true) {
-      case renderLoginForm && !authenticated:
-        renderLogin = <LoginForm submitFormHandler={this.onLogin} />;
-        break;
-      case !renderLoginForm && !authenticated:
-        renderLogin = (
-          <>
-            <button
-              id="login"
-              onClick={() => this.setState({ renderLoginForm: true })}
-            >
-              Login
-            </button>
-            <p id="message">{message}</p>
-          </>
-        );
-        break;
-      case authenticated:
-        renderLogin = (
-          <p id="message" >
-            Hi {JSON.parse(sessionStorage.getItem("credentials")).uid}
-          </p>
-        );
-        break;
-    }
-
+    const renderLogin = this.state.renderLoginForm ? (
+      <LoginForm submitFormHandler={this.onLogin} />
+    ) : (
+      <button
+        id="login"
+        onClick={() => this.setState({ renderLoginForm: true })}
+      >
+        Login
+      </button>
+      
+    );
     return (
       <>
         <InputFields onChangeHandler={this.onChangeHandler} />
-        { renderLogin }
+        {renderLogin}
+
         <DisplayCooperResult
           distance={this.state.distance}
           gender={this.state.gender}
@@ -73,5 +57,4 @@ class App extends Component {
     );
   }
 }
-
 export default App;
--- src/modules/auth.js
+++ src/modules/auth.js
@@ -3,14 +3,14 @@ import axios from "axios";
 const authenticate = async (email, password) => {
   try {
     const response = await axios.post("/auth/sign_in", {
-      email:  email,
+      email: email,
       password: password
     });
     await storeAuthCredentials(response);
     return { authenticated: true };
   } catch (error) {
     return { authenticated: false, message: error.response.data.errors[0] };
-  } 
+  }
 };
 
 const storeAuthCredentials = ({ headers }) => {
